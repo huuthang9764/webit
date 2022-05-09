8caa210f5bfae4f88af9128e21bf382b066a420b
Nguyễn Hữu Thắng <>
2022-05-09 10:53:53 +0700

    0905

--- src/components/Contents/Content/content.js
+++ src/components/Contents/Content/content.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import ProductApi from "service/Product";
 import Content_item from "../Content_item/Content_item";
 import "./content.scss";
 
 const Contents = () => {
+  const [products, setProducts] = useState([]);
+
+  // axios({
+  //   method: "GET",
+  //   url: ProductApi.getAll(),
+  //   headers: {
+  //     "content-type": "application/json",
+  //   },
+  //   data: null,
+  // })
+  //   .then((res) => {
+  //     console.log(res.data);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await ProductApi.getAll();
+        console.log(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
   return (
     <div className="Container">
       <section>
@@ -10,15 +40,7 @@ const Contents = () => {
           <h2>Bàn phím</h2>
         </div>
         <div className="box-products">
-          <Content_item />
-        </div>
-      </section>
-      <section>
-        <div className="title-product">
-          <h2>Bàn phím</h2>
-        </div>
-        <div className="box-products">
-          <Content_item />
+          {/* <Content_item products={products} /> */}
         </div>
       </section>
     </div>
--- src/components/Contents/Content_item/Content_item.js
+++ src/components/Contents/Content_item/Content_item.js
@@ -1,215 +1,34 @@
 import React from "react";
+
 import "./content_item.scss";
-export default function Content_item() {
+export default function Content_item(products) {
   return (
     <div className="list-products">
-      <div className="item-product">
-        <div className="item-product__box-img">
-          <a href="#">
-            <img
-              className="cpslazy loaded"
-              alt="iPhone 13 Pro Max 128GB | Chính hãng VN/A"
-              data-ll-status="loaded"
-              src="https://anphat.com.vn/media/product/250_40178_logitech_g_pro_x_league_of_legends.jpg"
-            />
-          </a>
+      {products.map((item, i) => (
+        <div className="item-product" key={i}>
+          <div className="item-product__box-img">
+            <a href="#">
+              <img
+                className="cpslazy loaded"
+                alt="iPhone 13 Pro Max 128GB | Chính hãng VN/A"
+                data-ll-status="loaded"
+                src={item.url_product}
+              />
+            </a>
+          </div>
+          <div className="item-product__box-name">
+            <a href="#">
+              <p>{item.name_product}</p>
+            </a>
+          </div>
+          <div className="item-product__box-price">
+            <p className="special-price">{item.price}</p>
+          </div>
+          <div className="item-product__box-detail">
+            <a href="/detail">Xem chi tiết</a>
+          </div>
         </div>
-        <div className="item-product__box-name">
-          <a href="#">
-            <p>Bàn phím cơ Logitech G Pro X League Of Legends Brown switch</p>
-          </a>
-        </div>
-        <div className="item-product__box-price">
-          <p className="special-price">2.499.000&nbsp;₫</p>
-        </div>
-        <div className="item-product__box-detail">
-          <a href="/detail">Xem chi tiết</a>
-        </div>
-      </div>
-      <div className="item-product">
-        <div className="item-product__box-img">
-          <a href="#">
-            <img
-              className="cpslazy loaded"
-              alt="iPhone 13 Pro Max 128GB | Chính hãng VN/A"
-              data-ll-status="loaded"
-              src="https://anphat.com.vn/media/product/250_40178_logitech_g_pro_x_league_of_legends.jpg"
-            />
-          </a>
-        </div>
-        <div className="item-product__box-name">
-          <a href="#">
-            <p>Bàn phím cơ Logitech G Pro X League Of Legends Brown switch</p>
-          </a>
-        </div>
-        <div className="item-product__box-price">
-          <p className="special-price">2.499.000&nbsp;₫</p>
-        </div>
-        <div className="item-product__box-detail">
-          <a href="/detail">Xem chi tiết</a>
-        </div>
-      </div>
-      <div className="item-product">
-        <div className="item-product__box-img">
-          <a href="#">
-            <img
-              className="cpslazy loaded"
-              alt="iPhone 13 Pro Max 128GB | Chính hãng VN/A"
-              data-ll-status="loaded"
-              src="https://anphat.com.vn/media/product/250_40178_logitech_g_pro_x_league_of_legends.jpg"
-            />
-          </a>
-        </div>
-        <div className="item-product__box-name">
-          <a href="#">
-            <p>Bàn phím cơ Logitech G Pro X League Of Legends Brown switch</p>
-          </a>
-        </div>
-        <div className="item-product__box-price">
-          <p className="special-price">2.499.000&nbsp;₫</p>
-        </div>
-        <div className="item-product__box-detail">
-          <a href="/detail">Xem chi tiết</a>
-        </div>
-      </div>
-      <div className="item-product">
-        <div className="item-product__box-img">
-          <a href="#">
-            <img
-              className="cpslazy loaded"
-              alt="iPhone 13 Pro Max 128GB | Chính hãng VN/A"
-              data-ll-status="loaded"
-              src="https://anphat.com.vn/media/product/250_40178_logitech_g_pro_x_league_of_legends.jpg"
-            />
-          </a>
-        </div>
-        <div className="item-product__box-name">
-          <a href="#">
-            <p>Bàn phím cơ Logitech G Pro X League Of Legends Brown switch</p>
-          </a>
-        </div>
-        <div className="item-product__box-price">
-          <p className="special-price">2.499.000&nbsp;₫</p>
-        </div>
-        <div className="item-product__box-detail">
-          <a href="/detail">Xem chi tiết</a>
-        </div>
-      </div>
-      <div className="item-product">
-        <div className="item-product__box-img">
-          <a href="#">
-            <img
-              className="cpslazy loaded"
-              alt="iPhone 13 Pro Max 128GB | Chính hãng VN/A"
-              data-ll-status="loaded"
-              src="https://anphat.com.vn/media/product/250_40178_logitech_g_pro_x_league_of_legends.jpg"
-            />
-          </a>
-        </div>
-        <div className="item-product__box-name">
-          <a href="#">
-            <p>Bàn phím cơ Logitech G Pro X League Of Legends Brown switch</p>
-          </a>
-        </div>
-        <div className="item-product__box-price">
-          <p className="special-price">2.499.000&nbsp;₫</p>
-        </div>
-        <div className="item-product__box-detail">
-          <a href="/detail">Xem chi tiết</a>
-        </div>
-      </div>
-      <div className="item-product">
-        <div className="item-product__box-img">
-          <a href="#">
-            <img
-              className="cpslazy loaded"
-              alt="iPhone 13 Pro Max 128GB | Chính hãng VN/A"
-              data-ll-status="loaded"
-              src="https://anphat.com.vn/media/product/250_40178_logitech_g_pro_x_league_of_legends.jpg"
-            />
-          </a>
-        </div>
-        <div className="item-product__box-name">
-          <a href="#">
-            <p>Bàn phím cơ Logitech G Pro X League Of Legends Brown switch</p>
-          </a>
-        </div>
-        <div className="item-product__box-price">
-          <p className="special-price">2.499.000&nbsp;₫</p>
-        </div>
-        <div className="item-product__box-detail">
-          <a href="/detail">Xem chi tiết</a>
-        </div>
-      </div>
-      <div className="item-product">
-        <div className="item-product__box-img">
-          <a href="#">
-            <img
-              className="cpslazy loaded"
-              alt="iPhone 13 Pro Max 128GB | Chính hãng VN/A"
-              data-ll-status="loaded"
-              src="https://anphat.com.vn/media/product/250_40178_logitech_g_pro_x_league_of_legends.jpg"
-            />
-          </a>
-        </div>
-        <div className="item-product__box-name">
-          <a href="#">
-            <p>Bàn phím cơ Logitech G Pro X League Of Legends Brown switch</p>
-          </a>
-        </div>
-        <div className="item-product__box-price">
-          <p className="special-price">2.499.000&nbsp;₫</p>
-        </div>
-        <div className="item-product__box-detail">
-          <a href="/detail">Xem chi tiết</a>
-        </div>
-      </div>
-      <div className="item-product">
-        <div className="item-product__box-img">
-          <a href="#">
-            <img
-              className="cpslazy loaded"
-              alt="iPhone 13 Pro Max 128GB | Chính hãng VN/A"
-              data-ll-status="loaded"
-              src="https://anphat.com.vn/media/product/250_40178_logitech_g_pro_x_league_of_legends.jpg"
-            />
-          </a>
-        </div>
-        <div className="item-product__box-name">
-          <a href="#">
-            <p>Bàn phím cơ Logitech G Pro X League Of Legends Brown switch</p>
-          </a>
-        </div>
-        <div className="item-product__box-price">
-          <p className="special-price">2.499.000&nbsp;₫</p>
-        </div>
-        <div className="item-product__box-detail">
-          <a href="/detail">Xem chi tiết</a>
-        </div>
-      </div>
-      <div className="item-product">
-        <div className="item-product__box-img">
-          <a href="#">
-            <img
-              className="cpslazy loaded"
-              alt="iPhone 13 Pro Max 128GB | Chính hãng VN/A"
-              data-ll-status="loaded"
-              src="https://anphat.com.vn/media/product/250_40178_logitech_g_pro_x_league_of_legends.jpg"
-            />
-          </a>
-        </div>
-        <div className="item-product__box-name">
-          <a href="#">
-            <p>Bàn phím cơ Logitech G Pro X League Of Legends Brown switch</p>
-          </a>
-        </div>
-        <div className="item-product__box-price">
-          <p className="special-price">2.499.000&nbsp;₫</p>
-        </div>
-        <div className="item-product__box-detail">
-          <a href="/detail">Xem chi tiết</a>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
